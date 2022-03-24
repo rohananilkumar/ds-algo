8191e4bdeea41fae91426eafa4aacdecf0528567
ROHAN <ro.anilpnr@gmail.com>
2022-03-24 13:41:39 +0530

    graph implementation

---
+++ Graphs/graphImplementation.js
@@ -0,0 +1,13 @@
+// Edge List 
+const graphEdge = [[0,2], [2,3], [2,1], [1,3]];
+
+// Adjacency List 
+const graphAdjacency = [[2], [2,3], [0,1,3], [1,2]]; //index = node value
+
+// Adjacent Matrix 
+const graphAdjacencyMatrix = [
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [1, 1, 0, 1],
+    [0, 1, 1, 0],
+]
